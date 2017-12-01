12e5670cfec698a3cff89a8df3ca530d53b073d8
ilyiglesias <ilysa.iglesias@noaa.gov>
2017-12-01 14:02:01 -0800

    delete script

--- notes_ii.R
+++
@@ -1,5 +0,0 @@
-# you should commit --> push --> and then PULL (in case something has been updated)
-# note index.Rmb if you plan to publish it to a website 
-
-# pull --then commit -- then push 
-# try pulling that says you have locat things- comitt/ then pull.. then push
